56b84be89ede5c8e4f8d87f3894ff02774913ad1
yuan.zhou <492407250@qq.com>
2019-07-19 17:40:36 +0800

    20190719 T153

---
+++ src/main/java/history/T153.java
@@ -0,0 +1,28 @@
+package history;
+
+public class T153 {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0;
+        int right = nums.length-1;
+
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+
+        int mid = (left+right)/2;
+
+        while (right - left > 1) {
+            if (nums[mid] > nums[left]) {
+                left = mid;
+            }else{
+                right = mid;
+            }
+            mid = (left+right)/2;
+        }
+
+        return Math.min(nums[left],nums[right]);
+    }
+}
