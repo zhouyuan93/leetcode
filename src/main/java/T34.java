ab60dfdb0786e58975957d6d34e25b1bb0061bd1
yuan.zhou <492407250@qq.com>
2020-04-07 14:10:27 +0800

    20200405 T34 100% 执行用时 : 0 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 42.4 MB, 在所有 Java 提交中击败了85.75%的用户

---
+++ src/main/java/T34.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class T34 {
+
+    public int[] searchRange(int[] nums, int target) {
+        int minP = 0;
+        int maxP = nums.length - 1;
+        int[] notFoundRes = new int[]{-1, -1};
+
+        //特殊情况 1-数组长度为0
+        if (nums.length == 0) {
+            return notFoundRes;
+        }
+        //特殊情况 2-target不在目标数组范围内
+        if (nums[0] > target || nums[maxP] < target) {
+            return notFoundRes;
+        }
+
+        //临时指针
+        int temp = (minP + maxP) / 2;
+
+        //规则
+        //target可能在范围内！
+        //minP maxP 只指向临界数值
+        while ((nums[minP] != target || nums[maxP] != target) && maxP - minP > 1) {
+            if (nums[temp] > target) {
+                maxP = temp;
+                temp = (minP + maxP) / 2;
+            } else if (nums[temp] < target) {
+                minP = temp;
+                temp = (minP + maxP) / 2;
+            } else {
+                //nums[temp] == target 临界，非临界
+                if (nums[maxP] != target) {
+                    //循环始终保证nums[temp] == target
+                    int tempX = temp;
+                    while (maxP - tempX > 1) {
+                        int q = (tempX + maxP) / 2;
+                        if (nums[q] != target) {
+                            maxP = q;
+                        } else {
+                            tempX = q;
+                        }
+                    }
+                    maxP = nums[maxP] == target ? maxP : tempX;
+                }
+                if (nums[minP] != target) {
+                    //循环始终保证nums[temp] == target
+                    int tempX = temp;
+                    while (tempX - minP > 1) {
+                        int q = (tempX + minP) / 2;
+                        if (nums[q] != target) {
+                            minP = q;
+                        } else {
+                            tempX = q;
+                        }
+                    }
+                    minP = nums[minP] == target ? minP : tempX;
+                }
+            }
+
+        }
+
+        minP = nums[minP] == target ? minP : maxP;
+        maxP = nums[maxP] == target ? maxP : minP;
+
+        int[] res = new int[]{minP, maxP};
+        return nums[minP] == target ? res : notFoundRes;
+    }
+}
+
---
+++ src/test/java/T34Test.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class T34Test {
+
+    private T34 t;
+
+    @BeforeEach
+    void setUp() {
+        this.t = new T34();
+    }
+
+    @Test
+    public void test_7() {
+        int[] nums = {7,8};
+        int target =  7;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {0,0};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+    @Test
+    public void test_6() {
+        int[] nums = {7,7};
+        int target =  7;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {0,1};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+    @Test
+    public void test_5() {
+        int[] nums = {5,7,7,8,8,10};
+        int target =  9;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {-1,-1};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+    @Test
+    public void test_4() {
+        int[] nums = {5,7,7,8,8,10};
+        int target =  5;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {0, 0};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+    @Test
+    public void test_3() {
+        int[] nums = {5,7,7,8,8,10};
+        int target =  10;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {5, 5};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+    @Test
+    public void test_2() {
+        int[] nums = {5};
+        int target = 5;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {0, 0};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+    @Test
+    public void test_1() {
+        int[] nums = {5,7,7,8,8,10};
+        int target =  8;
+        int[] actual = t.searchRange(nums, target);
+        int[] except = {3, 4};
+        System.out.println(Arrays.toString(actual));
+        assertArrayEquals(except, actual);
+    }
+}
