426677c937b648bed80f603d1815b149325e989b
yuan_zhou <49247250@qq.com>
2020-04-20 23:57:35 +0800

    20200420 T107 99% 13min 执行用时 : 1 ms, 在所有 Java 提交中击败了99.64%的用户 内存消耗 : 40.3 MB, 在所有 Java 提交中击败了7.41%的用户

---
+++ src/main/java/v1/T107.java
@@ -0,0 +1,43 @@
+package v1;
+
+import common.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yuan.zhou
+ */
+public class T107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        mergeTreeNodeIntoList(0,root,res);
+
+        List<List<Integer>> reversRes = new ArrayList<>();
+
+        for (List<Integer> floor : res) {
+            reversRes.add(0,floor);
+        }
+        return reversRes;
+    }
+
+    public static void mergeTreeNodeIntoList(int floor, TreeNode head, List<List<Integer>> res) {
+        if (head == null) {
+            return;
+        }
+
+        if (floor + 1 > res.size()) {
+            List<Integer> newFloor = new ArrayList<>();
+            newFloor.add(head.val);
+            res.add(newFloor);
+        }else{
+            res.get(floor).add(head.val);
+        }
+
+        mergeTreeNodeIntoList(floor + 1, head.left, res);
+        mergeTreeNodeIntoList(floor + 1, head.right, res);
+
+    }
+}
