f8c5eabc1dc3351f924d0f064a9dd6d7d1d31438
yuan_zhou <49247250@qq.com>
2020-04-20 23:29:19 +0800

    20200420 T100 100% 8min 执行用时 : 0 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 36.6 MB, 在所有 Java 提交中击败了5.55%的用户

---
+++ src/main/java/v1/T100.java
@@ -0,0 +1,16 @@
+package v1;
+
+import common.tree.TreeNode;
+
+public class T100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        return p != null && q != null
+                && p.val == q.val
+                && isSameTree(p.left, q.left) && isSameTree(q.right, p.right);
+
+    }
+}
