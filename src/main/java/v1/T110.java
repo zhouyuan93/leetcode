5500e49fa7829622cc860d2c5de31e5e5d989c0a
yuan.zhou <492407250@qq.com>
2020-04-21 20:06:35 +0800

    20200417 T110 99% 8min 执行用时 : 1 ms, 在所有 Java 提交中击败了99.83%的用户 内存消耗 : 39.5 MB, 在所有 Java 提交中击败了29.41%的用户

---
+++ src/main/java/v1/T110.java
@@ -0,0 +1,30 @@
+package v1;
+
+import common.tree.TreeNode;
+
+public class T110 {
+    public boolean isBalanced(TreeNode root) {
+        return treeMaxHeight(root, 0) > -1;
+    }
+
+    public static int treeMaxHeight(TreeNode root, int floor) {
+        if (root == null) {
+            return floor;
+        }
+
+        int left = treeMaxHeight(root.left, floor + 1);
+
+        if (left == -1) {
+            return -1;
+        }
+
+        int right = treeMaxHeight(root.right, floor + 1);
+
+        if (right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }else{
+            return Math.max(left, right);
+        }
+
+    }
+}
