c9dd1c6acd8445b28485145ac5310422416acfec
yuan.zhou <492407250@qq.com>
2020-05-18 14:54:54 +0800

    20200518 T41 99% 54min 执行用时 : 1 ms, 在所有 Java 提交中击败了99.98%的用户 内存消耗 : 39.8 MB, 在所有 Java 提交中击败了12.86%的用户

---
+++ src/test/java/v1/T42Test.java
@@ -0,0 +1,31 @@
+package v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class T42Test {
+
+    T42 t;
+
+    @BeforeEach
+    void setUp() {
+        t = new T42();
+    }
+
+    @Test
+    void test_2() {
+        int[] height = new int[]{4,2,0,3,2,5};
+        int actual = t.trap(height);
+        int excepted = 9;
+        assertEquals(excepted, actual);
+    }
+    @Test
+    void test_1() {
+        int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int actual = t.trap(height);
+        int excepted = 6;
+        assertEquals(excepted, actual);
+    }
+}
