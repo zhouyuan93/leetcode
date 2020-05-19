e7e9cfb01382a840846a68a466a2b354e6f0b20e
yuan.zhou <492407250@qq.com>
2020-05-19 17:10:31 +0800

    20200519 T680 97% 30min 执行用时 : 7 ms, 在所有 Java 提交中击败了97.33%的用户 内存消耗 : 40.8 MB, 在所有 Java 提交中击败了6.67%的用户

---
+++ src/test/java/v1/T680Test.java
@@ -0,0 +1,58 @@
+package v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class T680Test {
+    T680 t;
+
+    @BeforeEach
+    void setUp() {
+        t = new T680();
+    }
+
+    @Test
+    void test_6() {
+        String s = "";
+        boolean actual = t.validPalindrome(s);
+        boolean excepted = true;
+        assertEquals(excepted,actual);
+    }
+    @Test
+    void test_5() {
+        String s = "i";
+        boolean actual = t.validPalindrome(s);
+        boolean excepted = true;
+        assertEquals(excepted,actual);
+    }
+    @Test
+    void test_4() {
+        String s = "ai";
+        boolean actual = t.validPalindrome(s);
+        boolean excepted = true;
+        assertEquals(excepted,actual);
+    }
+    @Test
+    void test_3() {
+        String s = "aie";
+        boolean actual = t.validPalindrome(s);
+        boolean excepted = false;
+        assertEquals(excepted,actual);
+    }
+    @Test
+    void test_2() {
+        String s = "abccea";
+        boolean actual = t.validPalindrome(s);
+        boolean excepted = false;
+        assertEquals(excepted,actual);
+    }
+    @Test
+    void test_1() {
+        String s = "abca";
+        boolean actual = t.validPalindrome(s);
+        boolean excepted = true;
+        assertEquals(excepted,actual);
+    }
+}
