d0c2c074d41fb41e45f2b4b18010f9306d0ac8f8
yuan.zhou <492407250@qq.com>
2020-05-26 23:32:16 +0800

    20200526 T287 100% 2h 执行用时 : 0 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 39.6 MB, 在所有 Java 提交中击败了6.67%的用户

---
+++ src/test/java/v1/T287Test.java
@@ -0,0 +1,39 @@
+package v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class T287Test {
+
+    T287 t;
+
+    @BeforeEach
+    void setUp() {
+        t = new T287();
+    }
+
+    @Test
+    void test_1() {
+        int[] nums = {3, 1, 3, 4, 2};
+        int actual = t.findDuplicate(nums);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_2() {
+        int[] nums = {2, 1, 3, 4, 2, 4, 5, 6, 7};
+        int actual = t.findDuplicate(nums);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void test_3() {
+        int[] nums = {2, 5, 9, 6, 9, 3, 8, 9, 7, 1};
+        int actual = t.findDuplicate(nums);
+        int expected = 9;
+        assertEquals(expected, actual);
+    }
+}
