29b1c04cc458e8c13158745e1b858bfb07d2ecd4
yuan.zhou <492407250@qq.com>
2020-05-27 09:59:20 +0800

    20200527 T1160 99% 20min 执行用时 : 1 ms, 在所有 Java 提交中击败了99.97%的用户 内存消耗 : 42.6 MB, 在所有 Java 提交中击败了73.81%的用户

---
+++ src/main/java/v1/T160.java
@@ -0,0 +1,44 @@
+package v1;
+
+import common.ListNode;
+
+public class T160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int aLen = 0;
+        int bLen = 0;
+
+        ListNode p = headA;
+        while (p != null) {
+            p = p.next;
+            aLen++;
+        }
+        p = headB;
+        while (p != null) {
+            p = p.next;
+            bLen++;
+        }
+
+
+        ListNode pB = headB;
+        ListNode pA = headA;
+        while (aLen > bLen) {
+            pA = pA.next;
+            aLen--;
+        }
+        while (bLen > aLen) {
+            pB = pB.next;
+            bLen--;
+        }
+
+        while (pA != null) {
+            if (pA == pB) {
+                return pA;
+            }
+            pA = pA.next;
+            pB = pB.next;
+        }
+
+        return null;
+
+    }
+}
