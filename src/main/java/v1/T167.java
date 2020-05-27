adbf64b5972f42289216418c4b77e3b1dfbb3546
yuan.zhou <492407250@qq.com>
2020-05-27 10:16:46 +0800

    20200527 T167 97% 13min 执行用时 : 1 ms, 在所有 Java 提交中击败了97.09%的用户 内存消耗 : 39.9 MB, 在所有 Java 提交中击败了6.67%的用户

---
+++ src/main/java/v1/T167.java
@@ -0,0 +1,25 @@
+package v1;
+
+/**
+ * @author yuan.zhou
+ */
+public class T167 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        int lastRight = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                lastRight = right--;
+            } else if (numbers[left] + numbers[right] < target) {
+                right = lastRight;
+                left++;
+            } else {
+                break;
+            }
+        }
+
+        return new int[]{left + 1, right + 1};
+    }
+}
