402ea48de42f3a105bb88b5f06716293884d16d1
yuan.zhou <492407250@qq.com>
2020-05-27 23:57:18 +0800

    20200527 T974 91% 1h 执行用时 : 4 ms, 在所有 Java 提交中击败了91.27%的用户 内存消耗 : 42.6 MB, 在所有 Java 提交中击败了100.00%的用户

---
+++ src/main/java/v1/T974.java
@@ -0,0 +1,16 @@
+package v1;
+
+public class T974 {
+    public int subarraysDivByK(int[] A, int K) {
+        int[] record = new int[K];
+        record[0] = 1;
+        int sum = 0 ;
+        int res = 0 ;
+        for (int i : A) {
+            sum+=i;
+            int r = (sum % K + K) % K;
+            res += record[K]++;
+        }
+        return res;
+    }
+}
