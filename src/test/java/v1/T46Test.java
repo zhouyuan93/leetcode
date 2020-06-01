997f2f6a173537038d64ff50a543d596b3b286be
yuan.zhou <492407250@qq.com>
2020-06-01 16:39:42 +0800

    20200601 T46 82% 30min 执行用时 : 2 ms, 在所有 Java 提交中击败了82.42%的用户 内存消耗 : 40 MB, 在所有 Java 提交中击败了7.32%的用户

---
+++ src/test/java/v1/T46Test.java
@@ -0,0 +1,36 @@
+package v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import v1.T46;
+
+import java.util.List;
+
+class T46Test {
+    T46 t ;
+
+    @BeforeEach
+    void setUp() {
+        t = new T46();
+    }
+
+    @Test
+    void test_1() {
+        int[] nums = {1, 2};
+        List<List<Integer>> actual = t.permute(nums);
+        actual.forEach(node -> System.out.println(node.toString()));
+    }
+
+    @Test
+    void test_2() {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> actual = t.permute(nums);
+        actual.forEach(node -> System.out.println(node.toString()));
+    }
+    @Test
+    void test_3() {
+        int[] nums = {1, 2, 3, 4};
+        List<List<Integer>> actual = t.permute(nums);
+        actual.forEach(node -> System.out.println(node.toString()));
+    }
+}
