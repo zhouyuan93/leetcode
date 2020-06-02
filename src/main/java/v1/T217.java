6c24e82a13ec2934a2331b25784ac2c32bda9b22
yuan.zhou <492407250@qq.com>
2020-06-02 20:53:04 +0800

    20200602 T217 55% 2min 执行用时 : 9 ms, 在所有 Java 提交中击败了55.73%的用户 内存消耗 : 46.3 MB, 在所有 Java 提交中击败了6.98%的用户

---
+++ src/main/java/v1/T217.java
@@ -0,0 +1,22 @@
+package v1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author yuan.zhou
+ */
+public class T217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> memory = new HashSet<>();
+
+        for (int num : nums) {
+            if (memory.contains(num)) {
+                return true;
+            }
+            memory.add(num);
+        }
+        return false;
+
+    }
+}
