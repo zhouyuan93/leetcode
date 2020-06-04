aff133de4461e5d1d0650edbd49754231fef3dee
yuan.zhou <492407250@qq.com>
2020-06-04 10:38:54 +0800

    20200604 T461 100% 1min 执行用时 : 0 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 35.9 MB, 在所有 Java 提交中击败了6.45%的用户

---
+++ src/main/java/v1/T461.java
@@ -0,0 +1,21 @@
+package v1;
+
+/**
+ * @author yuan.zhou
+ */
+public class T461 {
+    public int hammingDistance(int x, int y) {
+        x = x ^ y;
+
+        int count = 0;
+        while (x > 0) {
+            if ((x & 1) == 1) {
+                count++;
+            }
+            x >>= 1;
+        }
+
+        return count;
+
+    }
+}
