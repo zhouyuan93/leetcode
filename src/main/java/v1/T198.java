b43048513a3d03e57e1837eaf1cb4620b291ec9a
yuan.zhou <492407250@qq.com>
2020-06-04 18:44:05 +0800

    20200604 T198 100% 10min 执行用时 : 0 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 37.1 MB, 在所有 Java 提交中击败了6.52%的用户

---
+++ src/main/java/v1/T198.java
@@ -0,0 +1,27 @@
+package v1;
+
+/**
+ * @author yuan.zhou
+ */
+public class T198 {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        //保存取当前节点时的最大值
+        int[] py = new int[nums.length];
+        //保存不取当前节点的最大值
+        int[] pn = new int[nums.length];
+
+        //初始化第一个数字
+        py[0] = nums[0];
+        pn[0] = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            py[i] = nums[i] + pn[i - 1];
+            pn[i] = Math.max(pn[i - 1], py[i - 1]);
+        }
+
+        return Math.max(py[nums.length - 1], pn[nums.length - 1]);
+    }
+}
