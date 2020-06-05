86b65eb02aa66cc529d4003a96412034323ba03f
yuan.zhou <492407250@qq.com>
2020-06-05 11:32:15 +0800

    20200605 T257 100% 14min 执行用时 : 1 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 39.7 MB, 在所有 Java 提交中击败了7.69%的用户

---
+++ src/main/java/v1/T257.java
@@ -0,0 +1,58 @@
+package v1;
+
+import common.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yuan.zhou
+ */
+public class T257 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        if (root.left == null && root.right == null) {
+            res.add(String.valueOf(root.val));
+            return res;
+        }
+
+        StringBuilder leftHead = new StringBuilder();
+        leftHead.append(root.val);
+
+        StringBuilder rightHead = new StringBuilder();
+        rightHead.append(root.val);
+
+
+        binaryTreePaths(root.left, leftHead, res);
+        binaryTreePaths(root.right, rightHead, res);
+
+        return res;
+
+    }
+
+    public void binaryTreePaths(TreeNode root, StringBuilder head, List<String> res) {
+        if (root == null) {
+            return;
+        }
+        head.append("->");
+        head.append(root.val);
+        if (root.left == null && root.right == null) {
+            res.add(head.toString());
+            return;
+        }
+
+        StringBuilder leftHead = head;
+        StringBuilder rightHead;
+        if (root.left != null && root.right != null) {
+            rightHead = new StringBuilder(head.toString());
+        }else{
+            rightHead = head;
+        }
+
+        binaryTreePaths(root.left, leftHead, res);
+        binaryTreePaths(root.right, rightHead, res);
+    }
+}
