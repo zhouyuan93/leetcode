49c367a20f7b0a100bcbf78e919c3c1dec850292
yuan.zhou <492407250@qq.com>
2020-06-05 15:10:45 +0800

    20200605 T263 100% 3min 执行用时 : 1 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 37 MB, 在所有 Java 提交中击败了5.55%的用户

---
+++ src/main/java/v1/T263.java
@@ -0,0 +1,27 @@
+package v1;
+
+/**
+ * @author yuan.zhou
+ */
+public class T263 {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        //除2
+        while ((num & 1) == 0) {
+            num >>= 1;
+        }
+        //除3
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        //除5
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+
+        return num == 1;
+
+    }
+}
