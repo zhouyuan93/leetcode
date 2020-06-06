83c8eecaefcbd340b0024732bef0b2f8c865293d
yuan.zhou <492407250@qq.com>
2020-06-06 11:09:45 +0800

    20200606 T128 99% 3min 执行用时 : 2 ms, 在所有 Java 提交中击败了99.94%的用户 内存消耗 : 40.2 MB, 在所有 Java 提交中击败了8.33%的用户

---
+++ src/test/java/v1/T128Test.java
@@ -0,0 +1,31 @@
+package v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class T128Test {
+    T128 t;
+
+    @BeforeEach
+    void setUp() {
+        t = new T128();
+    }
+
+    @Test
+    void test_1() {
+        int[] nums = {0, -1};
+        int actual = t.longestConsecutive(nums);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_2() {
+        int[] nums = {1,2,0,1};
+        int actual = t.longestConsecutive(nums);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+}
