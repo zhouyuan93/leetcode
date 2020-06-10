ae93db18e537532a20c8576ebd697506221ca76a
yuan.zhou <492407250@qq.com>
2020-06-10 21:12:25 +0800

    20200610 T342 100% 14min 执行用时 : 1 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗 : 36.7 MB, 在所有 Java 提交中击败了7.69%的用户

---
+++ src/main/java/v1/T342.java
@@ -0,0 +1,10 @@
+package v1;
+
+/**
+ * @author yuan.zhou
+ */
+public class T342 {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && ((num & (num - 1)) == 0) && ((num & 0xaaaaaaaa) == 0);
+    }
+}
