c8b06eb2cef816af490b4174bfdc3d018a23b05c
yuan.zhou <492407250@qq.com>
2020-06-10 21:22:30 +0800

    20200610 T344 99% 4min 执行用时 : 3 ms, 在所有 Java 提交中击败了94.84%的用户 内存消耗 : 40.2 MB, 在所有 Java 提交中击败了10.00%的用户

---
+++ src/main/java/v1/T345.java
@@ -0,0 +1,37 @@
+package v1;
+
+/**
+ * @author yuan.zhou
+ */
+public class T345 {
+    public String reverseVowels(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char[] c = s.toCharArray();
+        int i = 0;
+        int j = c.length -1;
+        while (i < j) {
+            if (!isVowels(c[i])) {
+                i++;
+                continue;
+            }
+            if (!isVowels(c[j])) {
+                j--;
+                continue;
+            }
+            char temp = c[i];
+            c[i] = c[j];
+            c[j] = temp;
+            i++;
+            j--;
+        }
+
+        return String.valueOf(c);
+    }
+
+    public boolean isVowels(char c) {
+        return c == 'a' || c == 'e' || c == 'o' || c == 'i' || c == 'u' ||
+         c == 'A' || c == 'E' || c == 'O' || c == 'I' || c == 'U';
+    }
+}
