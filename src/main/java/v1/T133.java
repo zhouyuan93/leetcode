bb7f703e2d6b13b3edf375ce2ae760a7634a0ce7
yuan.zhou <492407250@qq.com>
2020-07-22 11:57:30 +0800

    20200722 T133 20% 执行用时： 40 ms, 在所有 Java 提交中击败了20.81%的用户 内存消耗： 40.2 MB, 在所有 Java 提交中击败了6.25%的用户

---
+++ src/main/java/v1/T133.java
@@ -0,0 +1,58 @@
+package v1;
+
+import common.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class T133 {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (node.neighbors == null || node.neighbors.size() == 0) {
+            Node res = new Node();
+            res.val = node.val;
+            return res;
+        }
+
+        //初始化第一个节点
+
+
+        Queue<Node> nextQueue = new LinkedBlockingQueue<>();
+        nextQueue.offer(node);
+
+        Map<Integer, Node> copyMap = new TreeMap<>();
+        Node res = new Node();
+        res.val = node.val;
+        copyMap.put(node.val, res);
+
+        while (!nextQueue.isEmpty()) {
+            Node p = nextQueue.poll();
+            Node copyP = copyMap.get(p.val);
+
+            for (Node neighbor : p.neighbors) {
+                Node n ;
+                if (copyMap.containsKey(neighbor.val)) {
+                    n = copyMap.get(neighbor.val);
+                }else{
+                    n = new Node();
+                    n.val = neighbor.val;
+                    copyMap.put(n.val, n);
+                    nextQueue.offer(neighbor);
+                }
+                copyP.neighbors.add(n);
+            }
+        }
+
+        return res;
+
+    }
+
+}
