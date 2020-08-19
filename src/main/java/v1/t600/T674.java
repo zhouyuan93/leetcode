dd52ec1f68ab3441b9896788d1e48cf36ebb6535
yuan.zhou <492407250@qq.com>
2020-08-19 21:32:01 +0800

    20200818 T674 91% 执行用时： 3 ms, 在所有 Java 提交中击败了91.69%的用户 内存消耗： 37.9 MB, 在所有 Java 提交中击败了61.48%的用户

---
+++ src/main/java/v1/t600/T674.java
@@ -0,0 +1,36 @@
+package v1.t600;
+
+/**
+ * @author yuan.zhou
+ */
+public class T674 {
+
+    public int countSubstrings(String s) {
+        int n = s.length();
+        StringBuffer t = new StringBuffer("$#");
+        for (int i = 0; i < n; ++i) {
+            t.append(s.charAt(i));
+            t.append('#');
+        }
+        n = t.length();
+        t.append('!');
+
+        int[] f = new int[n];
+        int iMax = 0, rMax = 0, res = 0;
+        for (int i = 1; i < n; ++i) {
+            f[i] = i <= rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;
+            while (t.charAt(i + f[i]) == t.charAt(i - f[i])) {
+                ++f[i];
+            }
+            if (i + f[i] - 1 > rMax) {
+                iMax = i;
+                rMax = i + f[i] - 1;
+            }
+            res += f[i] / 2;
+        }
+
+        return res;
+    }
+
+
+}
