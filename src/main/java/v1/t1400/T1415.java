2652b447b59d2e9854b37176e6386d29eb784b84
yuan.zhou <492407250@qq.com>
2020-09-01 11:55:31 +0800

    move

--- src/main/java/v1/t1400/T1415.java
+++ src/main/java/v1/t1400/T1415.java
@@ -1,4 +1,4 @@
-package v1.t1000;
+package v1.t1400;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/test/java/v1/t1400/T1415Test.java
+++ src/test/java/v1/t1400/T1415Test.java
@@ -1,7 +1,8 @@
-package v1.t1000;
+package v1.t1400;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import v1.t1400.T1415;
 
 import static org.junit.jupiter.api.Assertions.*;
 
