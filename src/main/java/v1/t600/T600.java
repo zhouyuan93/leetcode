319d5a5cb9cfd1de0e8368eb77e668ca5d895364
yuan.zhou <492407250@qq.com>
2021-02-25 18:18:37 +0800

    20210225 T600 100% 执行用时： 1 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗： 35.7 MB, 在所有 Java 提交中击败了34.83%的用户

--- src/main/java/v1/t600/T600.java
+++ src/main/java/v1/t600/T600.java
@@ -13,25 +13,23 @@ public class T600 {
 
         int len = binaryString.length();
 
-        int[] array = new int[Math.max(4, len)];
-        array[0] = 1;
-        array[1] = 2;
-        for (int i = 2; i < array.length; i++) {
-            array[i] = array[i - 1] + array[i - 2];
+        int[] dp = new int[Math.max(4, len)];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        int res = array[len - 1];
+        int res = dp[len - 1];
 
         int i = 1;
         for (; i < len; i++) {
             if (binaryString.charAt(i) == '1') {
+                res += dp[len - i - 1];
                 if (binaryString.charAt(i - 1) == '1') {
-                    res += array[len - i - 1];
                     break;
                 }
-                res += array[len - i - 1];
             }
-
         }
 
         if (i == len) {
