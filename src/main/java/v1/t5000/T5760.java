a85de2ca3c900f6e9f14592bd037b038f0a8feb6
yuan.zhou <492407250@qq.com>
2021-05-16 14:41:27 +0800

    20210516 T5760 100% 执行用时： 2 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗： 36.7 MB, 在所有 Java 提交中击败了100.00%的用户

---
+++ src/main/java/v1/t5000/T5760.java
@@ -0,0 +1,38 @@
+package v1.t5000;
+
+public class T5760 {
+    public int minSwaps(String s) {
+        int res1 = 0;
+        int res0 = 0;
+
+        int count0 = 0;
+        int count1 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            boolean isJi = i % 2 == 0;
+            if (c == '0') {
+                count0++;
+                if (isJi) {
+                    res1++;
+                }
+            } else {
+                count1++;
+                if (isJi) {
+                    res0++;
+                }
+            }
+        }
+
+        if (count0 == count1) {
+            return Math.min(res0, res1);
+        }
+
+        if (Math.abs(count0 - count1) > 1) {
+            return -1;
+        }
+
+        return count0 > count1 ? res0 : res1;
+
+    }
+}
