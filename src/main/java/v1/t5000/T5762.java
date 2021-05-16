d8310fbbf281d85f87c4724a08262288958538f1
yuan.zhou <492407250@qq.com>
2021-05-16 14:45:06 +0800

    20210514 T5762 100% 执行用时： 207 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗： 70.8 MB, 在所有 Java 提交中击败了100.00%的用户

---
+++ src/main/java/v1/t5000/T5762.java
@@ -0,0 +1,36 @@
+package v1.t5000;
+
+public class T5762 {
+    public static int MOD = 1_000_000_007;
+    public static Long[][] dp;
+
+    public int rearrangeSticks(int n, int k) {
+        dp = new Long[k + 1][n + 1];
+        for (int i = 0; i < k + 1; i++) {
+            dp[i][i] = 1L;
+        }
+
+
+        return (int) getDp(n, k);
+
+    }
+
+    public long getDp(int n, int k) {
+        if (n < 0 || k < 0) {
+            return 0;
+        }
+
+        if (dp[k][n] != null) {
+            return dp[k][n];
+        }
+
+
+        long res = getDp(n - 1, k) * (n - 1) % MOD;
+
+        res += getDp(n - 1, k - 1) % MOD;
+        res %= MOD;
+        dp[k][n] = res;
+
+        return res;
+    }
+}
