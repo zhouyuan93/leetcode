7c7575057c9aef5d5ab8af9032c572a7a8359c0b
yuan.zhou <492407250@qq.com>
2021-07-15 17:42:13 +0800

    20210715 T1476 98.90% 执行用时： 22 ms, 在所有 Java 提交中击败了98.90%的用户 内存消耗： 42.4 MB, 在所有 Java 提交中击败了23.35%的用户

---
+++ src/main/java/v1/t1400/T1476.java
@@ -0,0 +1,25 @@
+package v1.t1400;
+
+public class T1476 {
+    public static class SubrectangleQueries {
+
+        int[][] rectangle;
+
+        public SubrectangleQueries(int[][] rectangle) {
+            this.rectangle = rectangle;
+        }
+
+        public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+            for (int i = row1; i <= row2; i++) {
+                for (int j = col1; j <= col2; j++) {
+                    rectangle[i][j] = newValue;
+                }
+            }
+        }
+
+        public int getValue(int row, int col) {
+            return rectangle[row][col] ;
+
+        }
+    }
+}
