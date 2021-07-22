23889af97580e3037813d281be83fee8f793f619
yuan.zhou <492407250@qq.com>
2021-07-22 14:51:16 +0800

    20210722 T412 99.36% 执行用时： 1 ms, 在所有 Java 提交中击败了99.36%的用户 内存消耗： 39.4 MB, 在所有 Java 提交中击败了82.46%的用户

---
+++ src/main/java/v1/t400/T412.java
@@ -0,0 +1,25 @@
+package v1.t400;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class T412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                res.add( "FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add( "Fizz");
+            } else if (i % 5 == 0) {
+                res.add( "Buzz");
+            } else{
+                res.add(String.valueOf(i));
+            }
+        }
+
+        return res;
+
+    }
+}
