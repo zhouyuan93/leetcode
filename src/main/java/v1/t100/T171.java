6020b4fac9a0a8b55201cec99e8c26df49a52bc8
yuan.zhou <492407250@qq.com>
2021-07-30 10:52:26 +0800

    20210730 T171 100.00% 执行用时： 1 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗： 38 MB, 在所有 Java 提交中击败了97.16%的用户

--- src/main/java/v1/t100/T171.java
+++ src/main/java/v1/t100/T171.java
@@ -3,7 +3,7 @@ package v1.t100;
 public class T171 {
     private static int MULTI = 26;
 
-    public int titleToNumber(String s){
+    public int titleToNumber(String s) {
         if (s == null || "".equals(s)) {
             return 0;
         }
@@ -18,10 +18,22 @@ public class T171 {
     public int multiMethod(char s, int i) {
         int res = s - 'A' + 1;
         while (i-- > 0) {
-           res *= MULTI;
+            res *= MULTI;
         }
         return res;
     }
 
+    public int titleToNumber2(String str) {
+
+        int res = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            int x = str.charAt(i) - 'A' + 1;
+            res *= 26;
+            res += x;
+        }
+
+        return res;
+    }
 
 }
