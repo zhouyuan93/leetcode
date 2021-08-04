4b389f05a438875bf853efc1f9c8dcf5c09f876e
yuan.zhou <492407250@qq.com>
2021-08-04 18:15:03 +0800

    20210801 T611 90% 执行用时： 31 ms, 在所有 Java 提交中击败了90.34%的用户 内存消耗： 38.2 MB, 在所有 Java 提交中击败了24.29%的用户

---
+++ src/main/java/v1/t600/T611.java
@@ -0,0 +1,26 @@
+package v1.t600;
+
+import java.util.Arrays;
+
+public class T611 {
+    public int triangleNumber(int[] nums) {
+        if (nums.length < 3) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int ans = 0;
+        for (int i = 2; i < nums.length; i++) {
+            for (int j = i - 1, k = 0; k < j; j--) {
+                while (k < j && nums[k] + nums[j] <= nums[i]) {
+                    k++;
+                    ans += j - k;
+
+                }
+            }
+        }
+
+        return ans;
+    }
+}
