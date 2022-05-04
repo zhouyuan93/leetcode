073abd9fbb9a2616c6a8ffb4b318e7ff208f029e
yuan_zhou <492407250@qq.com>
2022-05-04 23:00:51 +0800

    20220504 T1823 48% 执行用时： 3 ms, 在所有 Java 提交中击败了48.97%的用户 内存消耗： 39 MB, 在所有 Java 提交中击败了22.17%的用户

--- src/main/java/v1/t1800/T1823.java
+++ src/main/java/v1/t1800/T1823.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class T1823 {
-    public int findTheWinner(int n, int k) {
+    public int findTheWinner2(int n, int k) {
         List<Integer> list = new ArrayList<>(n);
         for (int i = 1; i <= n; i++) {
             list.add(i);
@@ -19,6 +19,26 @@ public class T1823 {
             p = n;
             list.remove(n);
         }
+        return list.get(0);
+    }
+
+    public int findTheWinner(int n, int k) {
+        k--;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+
+        int p = 0;
+        while (list.size() > 1) {
+            int len = list.size();
+            p +=(k % len);
+            if (p >= len) {
+                p -= len;
+            }
+            list.remove(p);
+        }
+
         return list.get(0);
     }
 }
