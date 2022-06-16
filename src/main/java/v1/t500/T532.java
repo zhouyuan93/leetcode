18eadc3dc1c15821d6aee21504460d9c2a814a9f
yuan.zhou <492407250@qq.com>
2022-06-16 11:17:35 +0800

    20220616 T532 100% 执行用时： 4 ms, 在所有 Java 提交中击败了100.00%的用户 内存消耗： 41.5 MB, 在所有 Java 提交中击败了59.78%的用户

--- src/main/java/v1/t500/T532.java
+++ src/main/java/v1/t500/T532.java
@@ -1,11 +1,10 @@
 package v1.t500;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Map;
 
 public class T532 {
-    public int findPairs(int[] nums, int k) {
+    public int findPairs1(int[] nums, int k) {
         HashSet<Integer> set = new HashSet<>();
         HashSet<Integer> res = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -27,6 +26,59 @@ public class T532 {
         System.out.println(res.toString());
 
         return res.size();
+    }
+
+    public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        if (k == 0) {
+            return getKZero(nums);
+        }
+
+        //去重
+        int p = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[p]) {
+                p++;
+                nums[p] = nums[i];
+            }
+        }
+        p++;
+        int x = getLessThan(nums, k + 1, p);
+        int y = getLessThan(nums, k, p);
+        return x - y;
+    }
+
+    private int getKZero(int[] nums) {
+        boolean hasSame = false;
+        int p = 0;
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[p] == nums[i]) {
+                if (!hasSame) {
+                    ans++;
+                    hasSame = true;
+                }
+            } else {
+                p = i;
+                hasSame = false;
+            }
+        }
+        return ans;
+    }
+
+    int getLessThan(int[] nums, int k, int p) {
+        int ans = 0;
+        int n = p;
+        for (int i = 0, j = 1; i < p; i++) {
+            while (j < n && nums[j] - nums[i] < k) {
+                j++;
+            }
+            if (j > i) {
+                ans += j - i - 1;
+            }
+        }
+        return ans;
     }
 
 }
--- src/test/java/v1/t500/T532Test.java
+++ src/test/java/v1/t500/T532Test.java
@@ -25,6 +25,15 @@ class T532Test {
 
     @Test
     void test_2() {
+        int[] nums = {3, 4, 1, 5, 2};
+        int k = 2;
+        int actual = t.findPairs(nums, k);
+        int expected = 3;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void test_3() {
         int[] nums = {1, 3, 1, 5, 4};
         int k = 0;
         int actual = t.findPairs(nums, k);
@@ -33,11 +42,20 @@ class T532Test {
     }
 
     @Test
-    void test_3() {
+    void test_4() {
         int[] nums = {1,1,1,2,2};
         int k = 2;
         int actual = t.findPairs(nums, k);
         int expected = 0;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void test_5() {
+        int[] nums = {1,1,1,1,1};
+        int k = 0;
+        int actual = t.findPairs(nums, k);
+        int expected = 1;
         assertEquals(expected,actual);
     }
 }
