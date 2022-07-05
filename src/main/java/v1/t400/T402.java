a545e63c120ec4ca9f759ea20560442e74de32b9
yuan.zhou <492407250@qq.com>
2022-07-05 15:23:31 +0800

    20220705 T402 99% 执行用时： 2 ms, 在所有 Java 提交中击败了99.83%的用户 内存消耗：41.2 MB , 在所有 Java 提交中击败了95.17%的用户

--- src/main/java/v1/t400/T402.java
+++ src/main/java/v1/t400/T402.java
@@ -1,11 +1,40 @@
 package v1.t400;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author yuan.zhou
  */
 public class T402 {
 
     public String removeKdigits(String num, int k) {
+        if (num.length() == k) {
+            return "0";
+        }
+
+        char[] chars = num.toCharArray();
+        int i = -1;
+
+        for (int j = 0; j < chars.length; j++) {
+            while (i >= 0 && chars[i] > chars[j] && k > 0) {
+                i--;
+                k--;
+            }
+            chars[++i] = chars[j];
+        }
+        while (k > 0) {
+            i--;
+            k--;
+        }
+        int j = 0;
+        while (j < i && chars[j] == '0') {
+            j++;
+        }
+        return new String(chars, j, i - j + 1);
+    }
+
+    public String removeKdigits2(String num, int k) {
         if (num == null || num.length() == k) {
             return "0";
         }
@@ -24,11 +53,11 @@ public class T402 {
             return;
         }
         if (chars.length - begin == k) {
-            return ;
+            return;
         }
         int min = chars[begin] - '0';
         int minI = begin;
-        for (int i = begin + 1; i < begin + k +1 && min != 0; i++) {
+        for (int i = begin + 1; i < begin + k + 1 && min != 0; i++) {
             int x = chars[i] - '0';
             if (x < min) {
                 min = x;
