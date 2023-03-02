304953c543f9f9630df36796af20da4ec15214d9
yuan.zhou <492407250@qq.com>
2023-03-02 15:16:59 +0800

    20230302 MST05_02 100%

---
+++ src/main/java/v1/mst/MST05_02.java
@@ -0,0 +1,17 @@
+package v1.mst;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MST05_02 {
+    public String printBin(double num) {
+        StringBuilder sb = new StringBuilder("0.");
+        while (sb.length() <= 32 && num != 0) {
+            num *= 2;
+            int digit = (int) num;
+            sb.append(digit);
+            num -= digit;
+        }
+        return sb.length() <= 32 ? sb.toString() : "ERROR";
+    }
+}
