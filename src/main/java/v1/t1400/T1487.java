036c6843d10d448240fabc5c840fd18bf07e5038
yuan.zhou <492407250@qq.com>
2023-03-06 10:59:36 +0800

    20230306 T1653 93.41%

--- src/main/java/v1/t1400/T1487.java
+++ src/main/java/v1/t1400/T1487.java
@@ -10,19 +10,19 @@ public class T1487 {
         HashMap<String, Integer> map = new HashMap<>((int) (names.length / 0.75));
         String[] ans = new String[names.length];
 
-        int i=0;
+        int i = 0;
         for (String name : names) {
             if (map.containsKey(name)) {
                 int val = map.get(name);
                 String newName = name + "(" + val + ")";
-                while(map.containsKey(newName)){
+                while (map.containsKey(newName)) {
                     val++;
                     newName = name + "(" + val + ")";
                 }
                 map.put(name, val + 1);
                 map.put(newName, 1);
                 ans[i++] = newName;
-            }else{
+            } else {
                 map.put(name, 1);
                 ans[i++] = name;
             }
--- src/main/java/v1/t1600/T1653.java
+++ src/main/java/v1/t1600/T1653.java
@@ -28,4 +28,30 @@ public class T1653 {
         return res;
 
     }
+
+    public int minimumDeletions2(String s) {
+        char[] sc = s.toCharArray();
+        int a, b;
+
+        if (sc[0] == 'a') {
+            a = 0;
+            b = 1;
+        } else {
+            a = 1;
+            b = 0;
+        }
+
+        int len = sc.length;
+        for (int i = 1; i < len; i++) {
+            char c = sc[i];
+            if (c == 'a') {
+                b++;
+            } else {
+                b = Math.min(a, b);
+                a++;
+            }
+        }
+
+        return Math.min(a, b);
+    }
 }
