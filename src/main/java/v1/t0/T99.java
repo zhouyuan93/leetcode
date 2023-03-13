e2c6e16834ecd02858399474bc27b51cec95dc88
yuan.zhou <492407250@qq.com>
2023-03-13 11:18:39 +0800

    20230313 T99 100%

--- src/main/java/v1/t0/T99.java
+++ src/main/java/v1/t0/T99.java
@@ -8,31 +8,36 @@ import java.util.stream.Collectors;
 
 public class T99 {
     public void recoverTree(TreeNode root) {
-        List<TreeNode> list = new ArrayList<>();
+        TreeNode[] e = new TreeNode[3];
         TreeNode[] m = new TreeNode[1];
 
-        find(root, list, m);
+        find(root, e, m);
 
-        TreeNode e1 = list.get(0);
-        TreeNode e2 = list.get(list.size() - 1);
+        TreeNode e1 = e[0];
+        TreeNode e2 = e[1];
         int t = e1.val;
         e1.val = e2.val;
         e2.val = t;
     }
 
-    private void find(TreeNode root, List<TreeNode> list, TreeNode[] m) {
-        if (root == null) {
+    private void find(TreeNode root, TreeNode[] e, TreeNode[] m) {
+        if (root == null || e[2] != null) {
             return;
         }
-        find(root.left, list, m);
+        find(root.left, e, m);
         if (m[0] != null) {
             if (m[0].val > root.val) {
-                list.add(m[0]);
-                list.add(root);
+                e[1] = root;
+                if (e[0] == null) {
+                    e[0] = m[0];
+                }else{
+                    e[2] = root;
+                    return;
+                }
             }
         }
         m[0] = root;
-        find(root.right, list, m);
+        find(root.right, e, m);
     }
 
 
