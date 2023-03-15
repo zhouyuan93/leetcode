dfb47c862a013adbf3b07df931dcf3f3101f4094
yuan.zhou <492407250@qq.com>
2023-03-15 14:50:36 +0800

    20230314 T84 63.31%

--- src/main/java/v1/t0/T84.java
+++ src/main/java/v1/t0/T84.java
@@ -1,7 +1,37 @@
 package v1.t0;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 public class T84 {
     public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+
+        int len = heights.length;
+
+        int[] h = new int[len + 2];
+
+        System.arraycopy(heights,0,h,1,len);
+
+        int res = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.addLast(0);
+        for (int i = 1; i < h.length; i++) {
+            while (h[i] < h[queue.peekLast()]) {
+                int height = h[queue.pollLast()];
+                int width = i - queue.peekLast() - 1;
+                res = Math.max(res, height * width);
+            }
+            queue.addLast(i);
+        }
+        return res;
+    }
+
+
+    public int largestRectangleArea2(int[] heights) {
         int[] stack = new int[heights.length + 1];
         stack[0] = -1;
         int p = 0;
