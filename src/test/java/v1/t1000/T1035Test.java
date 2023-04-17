7feb5a9505224f760512fe8d2697b296e871d0c3
yuan.zhou <492407250@qq.com>
2023-04-17 11:34:46 +0800

    move

--- src/test/java/v1/t1000/T1035Test.java
+++ src/test/java/v1/t1000/T1035Test.java
@@ -51,4 +51,42 @@ class T1035Test {
         int expected = 0;
         assertEquals(expected, actual);
     }
+
+    @Test
+    void test_21() {
+        int[] A = {1, 4, 2};
+        int[] B = {1, 2, 4};
+        int actual = t.maxUncrossedLines(A, B);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void test_22() {
+        int[] A = {2, 5, 1, 2, 5};
+        int[] B = {10, 5, 2, 1, 5, 2};
+        int actual = t.maxUncrossedLines(A, B);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void test_23() {
+        int[] A = {1, 3, 7, 1, 7, 5};
+        int[] B = {1, 9, 2, 5, 1};
+        int actual = t.maxUncrossedLines(A, B);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_24() {
+        int[] A = {1, 3, 7, 1, 7, 5};
+        int[] B = {};
+        int actual = t.maxUncrossedLines(A, B);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
